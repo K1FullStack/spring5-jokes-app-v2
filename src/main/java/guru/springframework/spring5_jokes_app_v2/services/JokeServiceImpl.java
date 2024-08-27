0807013bf1810c9b233b48bdc2b4d0e991599b99
K1 <k1fullstackk@gmail.com>
2024-08-27 20:07:50 +0300

    added html template, and no service found fix

--- src/main/java/guru/springframework/spring5_jokes_app_v2/services/JokeServiceImpl.java
+++ src/main/java/guru/springframework/spring5_jokes_app_v2/services/JokeServiceImpl.java
@@ -1,7 +1,9 @@
 package guru.springframework.spring5_jokes_app_v2.services;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
 
+@Service
 public class JokeServiceImpl implements JokeService {
   private final ChuckNorrisQuotes chuckNorrisQuotes;
 
